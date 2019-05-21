5e000671bf410fd428f6bbf4925ac9b03bfcb194
codepoin <codepoin.id@gmail.com>
2019-05-22 01:51:21 +0700

    Updates

---
+++ precache-manifest.f5c1703b32782da7457541af4183de22.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29f253ffc4a69650a23bac887dd07f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8296225b3874478524ab",
+    "url": "/static/css/2.2a88c607.chunk.css"
+  },
+  {
+    "revision": "71f1bf0d6627b084a61b",
+    "url": "/static/css/main.ca6043fe.chunk.css"
+  },
+  {
+    "revision": "8296225b3874478524ab",
+    "url": "/static/js/2.991505b9.chunk.js"
+  },
+  {
+    "revision": "71f1bf0d6627b084a61b",
+    "url": "/static/js/main.80981766.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "a655ba64f13df138ceb90767d6f75d88",
+    "url": "/static/media/rocket.a655ba64.png"
+  }
+]);
